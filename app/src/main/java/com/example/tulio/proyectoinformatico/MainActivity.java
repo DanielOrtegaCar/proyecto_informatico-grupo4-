11f53513304e960a510164b4222e7c95b50b23b1
JulioZapataD <julzapata@udec.cl>
2018-09-30 22:36:33 -0300

    Prueba N°4 git

--- app/src/main/java/com/example/tulio/proyectoinformatico/MainActivity.java
+++ app/src/main/java/com/example/tulio/proyectoinformatico/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.tulio.proyectoinformatico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton bsqt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bsqt= (ImageButton)findViewById(R.id.BotonBasquetbol);
+
+        bsqt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pantalla = new Intent(MainActivity.this,basquet_activity.class);
+                startActivity(pantalla);
+                finish();
+            }
+        });
+
     }
 }
