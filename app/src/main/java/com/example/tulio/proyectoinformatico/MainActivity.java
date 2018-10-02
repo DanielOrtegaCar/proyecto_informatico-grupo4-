2093e222094a46cc729a5024779c9b58b3949e84
Daniel Ortega Carcamo <18015929+DanielOrtega94@users.noreply.github.com>
2018-10-02 11:50:13 -0300

    Coneccion a la base de datos lista

--- app/src/main/java/com/example/tulio/proyectoinformatico/MainActivity.java
+++ app/src/main/java/com/example/tulio/proyectoinformatico/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.tulio.proyectoinformatico;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton bsqt;
+    Retrofit cliente;
+    ApiService apiService;
+    Usuario usuario;
+    List<Usuario> listaUsuarios;
 
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,37 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+*/
+        /////
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            cliente= new Retrofit.Builder().baseUrl(ApiService.URL).addConverterFactory(GsonConverterFactory.create()).build();
+            apiService=cliente.create(ApiService.class);
+            apiService.listaUsuarios().enqueue(new Callback<List<Usuario>>() {
+                @Override
+                public void onResponse(Call<List<Usuario>> call, Response<List<Usuario>> response) {
+                    Log.i("Cliente","Cliente Android");
+                    if (response.isSuccessful()){
+                        listaUsuarios = response.body();
+                        for (Usuario usuario:listaUsuarios){
+                            Log.i("TEXTO!!!",usuario.toString());
+                        }
+                    }
+                }
+                @Override
+                public void onFailure(Call<List<Usuario>> call, Throwable t) {
+                    Log.i("ERRRORRRRR!!!",t.getMessage());
+
+                }
+            });
+
+
+        }
+
 
     }
-}
+
